f07b86f3ec9753f6b1b764d58ef7c9a24d0fb121
violet <violettalemwa2@gmail.com>
2026-03-24 11:38:02 +0300

    Added compound interest calculator files

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ function calculate(){
     const yearsinput = document.getElementById("years");
     const total = document.getElementById("total")
 
-    let principal = principalinput.value;
-    let rate = rateinput.value;
-    let years = yearsinput.value;
-    const result = principal *Math.pow((rate/100+1), years);
+    let principal = Number(principalinput.value);
+    let rate = Number(rateinput.value);
+    let years = Number(yearsinput.value);
+    
 
     if(principal<0 || isNaN(principal)){
         principal=0;
-        principal.textContent=0;
+        principalinput.value=0;
     }
     if(rate<0 || isNaN(rate)){
         rate=0;
-        rate.textContent=0;
+        rateinput.value=0;
     }
     if(years<0 || isNaN(years)){
         years=0;
-        years.textContent=0;
+        yearsinput.value=0;
     }
+    const result = principal *Math.pow((rate/100+1), years);
     total.textContent=result.toLocaleString(undefined, {style:"currency", currency:"USD"});
 }
